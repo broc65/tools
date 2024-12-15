2dcb00c3a81f1a67313346303dcdc25708457122
broc <126756992+broc65@users.noreply.github.com>
2024-12-15 19:02:43 +0700

    Update bench.sh

--- bench.sh
+++ bench.sh
@@ -74,8 +74,8 @@ speed_test() {
 }
 
 speed() {
-    speed_test '50798' 'GMEDIA, ID'
-    speed_test '13623' 'Singtel, SG'
+    speed_test '6612' 'FirstMedia, ID'
+    speed_test '2054' 'ViewQwest, SG'
     speed_test '21541' 'i3D.net, US'
     speed_test '50467' 'Verizon, JP'
     speed_test '232' 'Stocade, NL'
